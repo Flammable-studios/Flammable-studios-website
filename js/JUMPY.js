a9757d85e590370b3ec870355d7031f229facf07
kryptonkid <jacob.l.maloney@gmail.com>
2025-11-15 11:30:00 -0800

    I'm awesome

--- js/JUMPY.js
+++ js/JUMPY.js
@@ -4,6 +4,7 @@ let pY = 350;
 let pX2 = 1000;
 let pY2 = 1000;
 let win = 0;
+let myCanvas;
 grassXs = [0];
 //, 20, 30, 40, 50, 60,70, 80, 90, 100, 110, 120,130 ,140, 150, 160, 170, 180, 190, 200, 210, 220, 230 ,240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590, 600
 grassSpace = 0;
@@ -37,7 +38,8 @@ repeat2 = 0
 
 //sets up the canvas
 function setup() {
-  createCanvas(600, 400);
+  let myCanvas = createCanvas(600, 400);
+  myCanvas.parent('canvas-container');
   background(240);
   frameRate(60)
   rectMode(CENTER)
